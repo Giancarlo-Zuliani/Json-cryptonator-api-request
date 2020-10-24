76e67e3db9af2278b77c5ece080abc26de3d4403
Giancarlo Zuliani <slaigox@gmail.com>
2020-10-24 16:41:24 +0200

    BTC/EUR and BTC/USD exchange rate added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 
-var x;
+var btcusd;
+
+var btceur;
 
 var myHeaders = new Headers();
 myHeaders.append("Cookie", "__cfduid=d6145ff725aa5e9d592b4644c2ed399191603549207");
@@ -16,8 +18,16 @@ document.querySelector('button').addEventListener('click',function(){
   fetch("https://api.cryptonator.com/api/ticker/btc-usd", requestOptions)
   .then(response => response.text())
   .then(result => {
-    x = JSON.parse(result);
-    document.getElementById('h').innerHTML =parseInt(x.ticker.price).toFixed(2) + " BTC/USD";
+    btcusd = JSON.parse(result);
+    document.getElementById('usd').innerHTML =parseInt(btcusd.ticker.price).toFixed(2) + " BTC/USD";
+  })
+  .catch(error => console.log('error', error));
+  fetch("https://api.cryptonator.com/api/ticker/btc-eur", requestOptions)
+  .then(response => response.text())
+  .then(result => {
+    btceur = JSON.parse(result);
+    document.getElementById('eur').innerHTML =parseInt(btceur.ticker.price).toFixed(2) + " BTC/EUR";
+
   })
   .catch(error => console.log('error', error));
 })
